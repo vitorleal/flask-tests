ce6564f4a7985e62253abf65b69685571ccd17c3
Vitor Leal <vitorleal1@gmail.com>
2013-09-11 01:11:56 -0300

    initiated the ads module

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import datetime
 from app import db
 
+#Advertiser
 class Advertiser(db.Document):
     isActive     = db.BooleanField(default=False, required=True)
     first_name   = db.StringField(max_length=255, required=True)
@@ -21,3 +22,21 @@ class Advertiser(db.Document):
         'indexes': ['-created_at'],
         'ordering': ['-created_at']
     }
+
+#Categories
+class Categories(db.Document):
+  name = db.StringField(max_length=255, required=True)
+
+
+class ad(db.Document):
+  name       = db.StringField(max_length=255, required=True)
+  advertizer = db.ReferenceField('Advertiser')
+  start_date = db.DateTimeField(required=True)
+  end_date   = db.DateTimeField(required=True)
+  created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+  cpc        = db.StringField(max_length=255, required=True)
+  cpa        = db.StringField(max_length=255, required=True)
+  impressions= db.LongField(required=True)
+  taps       = db.LongField(required=True)
+  budget     = db.DecimalField(required=True)
+
