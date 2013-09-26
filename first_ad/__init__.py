863877ec5575101ebc7b258e32f26781d014e2ad
Vitor Leal <vitorleal1@gmail.com>
2013-09-26 00:29:01 -0300

    added gravatar to the ehader

--- first_ad/__init__.py
+++ first_ad/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine, MongoEngineSessionInterface
 from flask.ext.login import LoginManager
+from flask.ext.gravatar import Gravatar
 
 #Create the Flask app
 app = Flask(__name__, static_folder='../static', template_folder='../templates')
@@ -15,6 +16,12 @@ app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
+
+#gravatar
+gravatar = Gravatar(app, size=50, rating='g',
+                    default='retro', force_default=False,
+                    use_ssl=False, base_url=None,
+                  )
 
 #Connect to MongoLabs
 db = MongoEngine(app)
